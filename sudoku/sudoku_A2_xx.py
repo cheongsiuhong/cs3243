b56242d5b1dac83e8949020e646a68724aa4cbbb
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-10-16 15:26:20 +0800

    dfs with propagation, working

--- sudoku/sudoku_A2_xx.py
+++ sudoku/sudoku_A2_xx.py
@@ -1,21 +1,36 @@
 import sys
 import copy
-import deque
-
+from collections import deque
+import pprint
 
 class Sudoku(object):
 
-    ## 1. Assign all values that can be deterministically assigned based off
-    ##    what values peers in the same Unit have taken.
-    ## 3. Perform Back-Tracking Search
+    # 1. Assign all values that can be deterministically assigned based off
+    # what values peers in the same Unit have taken.
+    # 3. Perform Back-Tracking Search
 
     def __init__(self, puzzle):
         self.digits = '123456789'
-        self.squares = [(int(x), int(y)) for x in digits for y in digits]
+        # List of coordinates
+        self.squares = [(int(x), int(y))
+                        for x in self.digits for y in self.digits]
+        # All the units
+        self.rows = [[(int(x), int(y)) for x in self.digits]
+                     for y in self.digits]
+        self.cols = [[(int(x), int(y)) for y in self.digits]
+                     for x in self.digits]
+        self.blocks = [[(int(x), int(y)) for x in X for y in Y]
+                       for X in ('123', '456', '789') for Y in ('123', '456', '789')]
+
         # Puzzle: The current state of the puzzle
         self.puzzle = puzzle  # List of rows of integers [0-9], 0 == unfilled
+        self.ans = copy.deepcopy(puzzle)
+
         # square_domain: Dict of the domain of each square. Domain is a list
-        self.square_domain = initialise_domains()
+        self.square_domain = self.initialise_domains()
+        self.neighbour_domain = self.initialise_neighbours()
+
+#### INITIALIZATION ######################################################################
 
     # Returns a dictionary of Square => Domain of Square
     def initialise_domains(self):
@@ -25,8 +40,30 @@ class Sudoku(object):
                 # If a value was assigned
                 val = self.puzzle[row][col]
                 if val != 0:
-                    domain[(row, col)] = str(val)
+                    domain[(row + 1, col + 1)] = str(val)
         return domain
+
+    # Returns a dictionary of Square => Neighbours of Square
+    def initialise_neighbours(self):
+        nb = dict((square, self.enumerate_neighbours(square))
+                  for square in self.squares)
+        return nb
+
+    # Returns all neighbours of a square
+    def enumerate_neighbours(self, square):
+        row = [copy.deepcopy(r) for r in self.rows if square in r][0]
+        col = [copy.deepcopy(c) for c in self.cols if square in c][0]
+        block = [copy.deepcopy(b) for b in self.blocks if square in b][0]
+        row.remove(square)
+        col.remove(square)
+        block.remove(square)
+        return list(dict.fromkeys(row + col + block))
+
+    # Returns all neighbours of a square
+    def get_neighbours(self, square):
+        return self.neighbour_domain[square]
+
+#### UTILITY #################################################################################
 
     # Converts the square_domain dictionary to a puzzle (list of lists)
     def domains_to_puzzle(self):
@@ -51,39 +88,44 @@ class Sudoku(object):
             err_str = "Square {} has a domain: {}".format(square, domain)
             raise ValueError(err_str)
 
+    # Returns the size of a square's domain
+    def size_of_domain(self, square):
+        return len(self.square_domain[square])
+
+    # True if every domain has one and only one value
+    def goal_test(self, square_domain):
+        return all(len(square_domain[square]) == 1 for square in square_domain)
+
+    # True if any domain has no values
+    def unsolvable_test(self, square_domain):
+        return any(len(square_domain[square]) == 0 for square in square_domain)
+
+#### PREPROCESSING STEPS ##################################################################
+
     # AC-3 Algorithm that updates puzzle and square_domain in place
     # Constraint: AllDiff in all units
     def propagate_constraints(self):
         assigned_queue = deque(self.get_all_assigned_squares())
-        while len(queue) > 0:
-            square = queue.popleft()
+        while len(assigned_queue) > 0:
+            square = assigned_queue.popleft()
             square_val = self.value_of(square)
             neighbours = self.get_neighbours(square)
             # Only consider updated_neighbours to avoid infinite loop
-            updated_neighbours = self.shrink_domains(neighbours, square_val)       # This should raise an error if domain goes to zero for any of the squares
+            # This should raise an error if domain goes to zero for any of the squares
+            updated_neighbours = self.shrink_domains(neighbours, square_val)
             for neighbour in updated_neighbours:
-                if self.size_of_domain(neighbour) == 1:
-                    assigned_queue.append(neighbour)
+                assigned_queue.append(neighbour)
         # Update self.puzzle
         self.puzzle = self.domains_to_puzzle()
 
     def get_all_assigned_squares(self):
         assigned_squares = []
-        for row in range(9):
-            for col in range(9):
+        for row in range(1, 10):
+            for col in range(1, 10):
                 square = (row, col)
                 if len(self.square_domain[square]) == 1:
                     assigned_squares.append(square)
         return assigned_squares
-
-    def get_neighbours(self, square):
-        row     = filter(lambda x: square in x, self.get_rows())[0]
-        col     = filter(lambda x: square in x, self.get_cols())[0]
-        block   = filter(lambda x: square in x, self.get_blocks())[0]
-        row.remove(square)
-        col.remove(square)
-        block.remove(square)
-        return row + col + block
 
     def shrink_domains(self, neighbours, value):
         updated_neighbours = []
@@ -91,111 +133,52 @@ class Sudoku(object):
             domain = self.square_domain[neighbour]
             if str(value) in domain:
                 self.square_domain[neighbour] = domain.replace(str(value), '')
-                updated_neighbours.append(neighbour)
+                if self.size_of_domain(neighbour) == 1:
+                    updated_neighbours.append(neighbour)
         return updated_neighbours
 
-    def size_of_domain(self, neighbour):
-        return len(self.square_domain[neighbour])
+#### Searching ####################################################################
 
-    def backtrack_search(self, state, domains):
-        # The graph has been preprocessed
-        # Get all the squares that have not been assigned sort in descending order
-        incomplete_squares = self.get_incomplete_squares(state)  ########## If any domains are empty, return an empty list
-        if len(incomplete_squares) == 0:
-            return self.goal_test(state)
-        square = incomplete_squares.pop()
-        for value in domains[square]:
-            new_state = copy.deepcopy(state)
-            new_domains = copy.deepcopy(domains)
-            new_state[square[0]][square[1]] = int(value)    # type(value): str
-            new_domains[square] = value                     # type(value): str
-            # Create a new copy where this value has been assigned to the Square
-            # Call backtrack_search
-            # If true, return state
-        pass
+    # Returns domains if success, False otherwise
+    def search(self, domains):
+        if self.unsolvable_test(domains):  # Failed
+            return False
 
+        if self.goal_test(domains):  # Done
+            return domains
+
+        # Get square with the least available value
+        num_available_values, square_to_assign = min(self.get_unassigned_squares(domains))
+        available_values = domains[square_to_assign]
+
+        for digit in available_values:
+            domains[square_to_assign] = digit
+            new_domains = self.shrink_domains_search(
+                self.get_neighbours(square_to_assign), digit, domains.copy())
+            result = self.search(new_domains)
+            if result:
+                return result
+
+    # Returns domains after propagation
+    def shrink_domains_search(self, neighbours, digit, domains):
+        for neighbour in neighbours:
+            domain = domains[neighbour]
+            if digit in domain:
+                domains[neighbour] = domain.replace(digit, '')
+                if len(domains[neighbour]) == 1:
+                    self.shrink_domains_search(self.get_neighbours(neighbour), domains[neighbour], domains)
+        return domains
+
+    def get_unassigned_squares(self, domains):
+        return [(len(domains[square]), square) for square in domains if len(domains[square]) > 1]
 
     def solve(self):
         self.propagate_constraints()    # AC-3 algorithm: Preprocessing
+        self.square_domain = self.search(self.square_domain.copy())
+        self.ans = self.domains_to_puzzle()
         # Backtracking (DFS) algorithm
-        state, domains = self.backtrack_search(self.puzzle, self.square_domain)
-
-
-
-    # (True, State) if self.puzzle is a valid sudoku state, else (False, State)
-    # Does NOT check for uniqueness of values
-    def goal_test(self, state):
-        # All units must sum to exactly 45: sum([1 to 9])
-        # Check if rows sum to 45
-        row_valid = all(map(lambda x: sum(x) == 45, self.get_rows(state)))
-        # Check if columns sum to 45
-        col_valid = all(map(lambda x: sum(x) == 45, self.get_cols(state)))
-        # Check if blocks sum to 45
-        blocks_valid = all(map(lambda x: sum(x) == 45, self.get_blocks(state)))
-        return row_valid and col_valid and blocks_valid, state
-
-    # Transpose a Matrix represented by a list of lists
-    def transpose(self, matrix):
-        return list(zip(*matrix))
-
-    def get_rows(self, state):
-        return copy.deepcopy(state)
-
-    def get_cols(self, state):
-        return self.transpose(self.get_rows(state))
-
-    def get_blocks(self, state):
-        block_indexes = [] # List of lists of indexes for a block
-        for X in ('123', '456', '789'):
-            for Y in ('123', '456', '789'):
-                block_indexes.append([(int(x), int(y)) for x in X for y in Y])
-        blocks = []
-        for indexes in block_indexes:
-            block = []
-            for row, col in indexes:
-                block.append(state[row][col])
-            blocks.append(block)
-        return blocks
-
-    ############################# Siuhongs Methods #############################
-
-    # def solve(self):
-    #     values = dict((square, digits) for square in squares)
-    #
-    #     for i in range(9):
-    #         for j in range(9):
-    #             digit = str(self.puzzle[i][j])
-    #             if digit != '0':
-    #                 self.assign((i + 1, j + 1), digit, values)
-    #
-    #     self.ans = self.values_to_puzzle(values)
-    #     return self.ans
-
-    def assign(self, square, number, values):
-        rest = values[square].replace(number, '')
-        for digit in rest:
-            if not self.eliminate(square, digit, values):
-                return False
-        return values
-
-    def eliminate(self, square, number, values):
-        # Number not in domain
-        if number not in values[square]:
-            return values
-        # Remove number from domain
-        values[square] = values[square].replace(number, '')
-        # Domain is empty after removal
-        if len(values[square]) == 0:
-            return False
-        # Domain has only one value left, remove the value from domains of all peers
-        if len(values[square]) == 1:
-            last_digit = values[square]
-            for peer in peers[square]:
-                if not self.eliminate(peer,last_digit, values):
-                    return False
-
-        return values
-
+        # state, domains = self.backtrack_search(self.puzzle, self.square_domain)
+        return self.ans
 
 
 if __name__ == "__main__":
